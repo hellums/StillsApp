0899e19e8814d2a0761981ba62c48197af80bc22
duane <hellums@yahoo.com>
2022-10-14 17:33:29 -0400

    Reorganize file structure

--- StillsApp/Program.cs
+++ StillsApp/Program.cs
@@ -12,7 +12,12 @@ namespace StillsApp
 
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+            {
+                Args = args,
+                // Look for static files in "UI/wwwroot" folder
+                WebRootPath = "UI/wwwroot"
+            });
             var env = builder.Environment;
             var services = builder.Services;
 
@@ -30,7 +35,7 @@ namespace StillsApp
 
             services.AddRazorPages();
             services.Configure<RazorPagesOptions>
-            (options => options.RootDirectory = "/UI/Pages");
+                (options => options.RootDirectory = "/UI/Pages");
     
         var app = builder.Build();
 
@@ -43,15 +48,16 @@ namespace StillsApp
             }
 
             app.MapControllers();
+
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parts API v1"));
+
+            app.UseStaticFiles();
             app.MapRazorPages();
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
             app.UseRouting();
             app.UseAuthorization();
-
 
             app.Run();
         }
