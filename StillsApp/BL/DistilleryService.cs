da2d9ca16c65bb7d94c9a40310ad5514df652fb6
hellums <83464025+hellums@users.noreply.github.com>
2022-10-15 08:36:37 -0400

    cleanup comments

--- StillsApp/BL/DistilleryService.cs
+++ StillsApp/BL/DistilleryService.cs
@@ -37,22 +37,8 @@ namespace StillsApp.BL
             return distillery;
         }
 
-        /*public Distillery GetPhoto(int id)
-        {
-            var distillery = _context.Distilleries
-                    .Include(distilleries => distilleries.Photos[0])
-                    .SingleOrDefault(i => i.Id == id);
-            //if (distillery == null) return NotFound();
-            return distillery;
-        }*/
         public String GetPhoto(int id)
         {
-            /*var distillery = _context.Distilleries
-                    .Include(distilleries => distilleries.Photos[0])
-                    .SingleOrDefault(i => i.Id == id);
-            //if (distillery == null) return NotFound();
-            var photo = distillery.Photos.FirstOrDefault().Base64Encoding;
-            return photo;*/
             var photo = _context.Photos.FirstOrDefault(i => i.Id == id);
             return photo.Base64Encoding.ToString();
         }
@@ -60,16 +46,6 @@ namespace StillsApp.BL
         public void Update(int id, Distillery model)
         {
             var distillery = _context.Distilleries.Find(id);
-
-            // validate
-            /*if (model.Username != user.Username && _context.Users.Any(x => x.Username == model.Username))
-                throw new AppException("Username '" + model.Username + "' is already taken");*/
-
-            // hash password if it was entered
-            /*if (!string.IsNullOrEmpty(model.Password))
-                user.PasswordHash = BCrypt.HashPassword(model.Password);*/
-
-            // copy model to user and save
             _context.Distilleries.Update(distillery);
             _context.SaveChanges();
         }
